e3ab262b4f295206cb572f6998e02f7cebabe8ae
ruchika <ruchika6698@gmail.com>
2020-05-04 14:48:18 +0530

    Added:Welcome to Quality Measurement

---
+++ QualityMeasurment/Conversion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QualityMeasurment
+{
+    public class Conversion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Quality Measurment problem");
+        }
+    }
+}
