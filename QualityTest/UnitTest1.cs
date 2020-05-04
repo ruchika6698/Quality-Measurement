272e84476d3409f34ecf71e34db062d0df6a3429
ruchika <ruchika6698@gmail.com>
2020-05-04 16:54:46 +0530

    Added:TC1.3:Test for comparision

--- QualityTest/UnitTest1.cs
+++ QualityTest/UnitTest1.cs
@@ -7,7 +7,24 @@ namespace QualityTest
     {
         Conversion converstion = new Conversion();
         /// <summary>
-        ///UC1: TC-1.1: Zero feet should return zero
+        ///UC1: TC-1.1: Test for comparision
+        /// </summary>
+        [Test]
+        public void GivenInchesAndFeets_CompareEquals_ReturnBool()
+        {
+            try
+            {
+                double inch = converstion.Feetmeasure("3");
+                Assert.AreEqual(36, inch);
+            }
+            catch (CustomException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        ///UC1: TC-1.2: Zero feet should return zero
         /// </summary>
         [Test]
         public void GivenZeroFeet_whenZeroFeet_shouldRetrunEqual()
@@ -16,10 +33,10 @@ namespace QualityTest
             Assert.AreEqual(0, inch);
         }
         /// <summary>
-        ///UC1: TC-1.2: Test of equality to check null input 
+        ///UC1: TC-1.3: Test of equality to check null input 
         /// </summary>
         [Test]
-        public void GivenZeroFeet_WhenNull_shouldReturnNullPointerException()
+        public void GivenZeroFeet_WhenCheckNull_shouldReturnNullPointerException()
         {
             try
             {
