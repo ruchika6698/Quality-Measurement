7b436d46538d228a1b445f867ac6043cd5225243
ruchika <ruchika6698@gmail.com>
2020-05-04 15:19:39 +0530

    Added:0 feet and 0 feet should return equal

--- QualityMeasurment/Conversion.cs
+++ QualityMeasurment/Conversion.cs
@@ -4,9 +4,27 @@ namespace QualityMeasurment
 {
     public class Conversion
     {
+        /// <summary>
+        /// Variable Declaration
+        /// </summary>
+        public int feet;
+
+        /// <summary>
+        /// Main Method
+        /// </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Quality Measurment problem");
+        }
+
+        /// <summary>
+        /// Method to Feet
+        /// </summary>
+        /// <param name="input"> input </param>
+        /// <returns> Calculate Feet </returns>
+        public double Feetmeasure(int input)
+        {
+            return feet = 12 * input;
         }
     }
 }
--- QualityTest/UnitTest1.cs
+++ QualityTest/UnitTest1.cs
@@ -1,18 +1,18 @@
 using NUnit.Framework;
+using QualityMeasurment;
 
 namespace QualityTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+        /// <summary>
+        ///UC1: TC-1.1: Zero feet should return zero
+        /// </summary>
         [Test]
-        public void Test1()
+        public void GivenZeroFeet_whenZeroFeet_shouldRetrunEqual()
         {
-            Assert.Pass();
+            Conversion converstion = new Conversion();
+            Assert.AreEqual(0, converstion.Feetmeasure(0));
         }
     }
 }
