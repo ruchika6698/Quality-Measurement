544408eee2d413d8926acdae1959c22e4f2a1570
ruchika <ruchika6698@gmail.com>
2020-05-04 18:16:31 +0530

    Added:UC1:Compare Feet and Inches

--- QualityMeasurment/Conversion.cs
+++ QualityMeasurment/Conversion.cs
@@ -25,21 +25,25 @@ namespace QualityMeasurment
         public double Feetmeasure(string input)
         {
             double feet;
+            // given 1ft=12 inch
             double inch = 12;
-
+            //if input type is not equal to null
             if (input != null)
             {
+                // Calculate feet value
                 if (double.TryParse(input, out feet))
                 {
                     feet = feet * inch;
                 }
                 else
                 {
+                    //else throw Type Not Match exception
                     throw new CustomException("Type Not Match",CustomException.ExceptionType.TYPE_NOT_MATCH);
                 }
             }
             else
             {
+                //else throw Null exception
                 throw new CustomException("Null",CustomException.ExceptionType.INPUT_NULL);
             }
             return feet;
